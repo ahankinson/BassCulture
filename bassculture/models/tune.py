425bd2177d7f1c9202fff6fc25844ea11e64cd81
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-06-30 17:23:43 +0100

    Updated tune model

--- bassculture/models/tune.py
+++ bassculture/models/tune.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 
 
@@ -5,9 +6,13 @@ class Tune(models.Model):
     class Meta:
         app_label = "bassculture"
 
+    def upload_to(instance, filename):
+        return "{0}".format(os.path.join('Af36', 'mei', filename))
+        return "{0}".format(instance.item.shelfmark)
+
     name = models.CharField(max_length=256)
     position = models.IntegerField(blank=True, null=True)
-    mei_file = models.FilePathField(blank=True, null=True)
+    mei_file = models.FileField(upload_to=upload_to, blank=True, null=True)
     start_page = models.IntegerField(blank=True, null=True)
     alternate_spellings = models.CharField(max_length=256, blank=True, null=True)
     item = models.ForeignKey("bassculture.Item", blank=True, null=True)
