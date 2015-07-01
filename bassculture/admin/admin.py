0bee24a066c2fda536dfc1a6c39dd67afdb38d16
luca.guariento <luca.guariento@glasgow.ac.uk>
2015-07-01 11:02:15 +0100

    no message

--- bassculture/admin/admin.py
+++ bassculture/admin/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from bassculture.models.source import Source
 from bassculture.models.item import Item
 from bassculture.models.author import Author
-from bassculture.models.printer import Printer
-from bassculture.models.publisher import Publisher
 from bassculture.models.tune import Tune
 from django.forms import TextInput, Textarea
 from django.db import models
