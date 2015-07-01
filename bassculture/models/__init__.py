47f21b952cc71ec9a449cc669364e355316a6519
luca.guariento <luca.guariento@glasgow.ac.uk>
2015-07-01 11:01:20 +0100

    no message

--- bassculture/models/__init__.py
+++ bassculture/models/__init__.py
@@ -1,10 +1,4 @@
 from bassculture.models.item import Item
 from bassculture.models.source import Source
-<<<<<<< HEAD
 from bassculture.models.author import Author
-=======
-from bassculture.models.author import Author
-from bassculture.models.publisher import Publisher
-from bassculture.models.printer import Printer
 from bassculture.models.tune import Tune
->>>>>>> 1230e42d632d994b96ac570949b2208b8d48a77c
---
+++ bassculture/serializers/seller.py
@@ -0,0 +1,9 @@
+rom rest_framework import serializers
+from bassculture.models.seller import Seller
+
+
+class SellerSerializer(serializers.HyperlinkedModelSerializer):
+    name = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Seller
