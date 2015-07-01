12662a7a7edf2cf7ca2d12cf7645c625e932e585
luca.guariento <luca.guariento@glasgow.ac.uk>
2015-07-01 11:35:45 +0100

    no message

--- bassculture/models/author.py
+++ bassculture/models/author.py
@@ -6,6 +6,7 @@ class Author(models.Model):
         app_label = 'bassculture'
 
     name = models.CharField(max_length=255)
+    short_title = models.ManyToManyField("bassculture.Item", blank=True, related_name="items")
 
     def __str__(self):
         return u"{0}".format(self.name)
