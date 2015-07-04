7ce53f303fbcc7e2256258da8d42962fe5db90be
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-07-04 13:18:24 +0100

    'authors' is now 'author'

--- bassculture/management/commands/import_metadata.py
+++ bassculture/management/commands/import_metadata.py
@@ -6,8 +6,7 @@ from bassculture.models import Item, Source, Author
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('location',
-                            help="Location of the .csv file to import")
+        parser.add_argument('location', help="Location of the .csv file to import")
 
     def handle(self, *args, **kwargs):
         print(kwargs)
@@ -25,14 +24,18 @@ class Command(BaseCommand):
                 self.create_source(row)
                 self.create_item(row)
 
+
+
     def create_source(self, row):
         print("Cleaning source table")
+
 
         print("Creating source " + row['source_id'])
         author, auth_created = Author.objects.get_or_create(name=row['author'])
         if auth_created:
             author.biographical_info = row['biographical_info']
             author.save()
+
 
         if row['orientation'] == "Portrait":
             orientation = "p"
@@ -42,22 +45,22 @@ class Command(BaseCommand):
             orientation = None
 
         d = {
-            'source_id': row['source_id'],
-            'full_title': row['full_title'],
-            'short_title': row['short_title'].strip(),
-            'authors': author,
-            'description': row['source_description'],
-            'source_notes': row['source_detailed_notes'],
-            'publisher': row['published'],
-            'printer': row['printed'],
-            'edition': row['edition'],
-            'orientation': row['orientation'],
-            'date': row['date'],
-            'link': row['link_url'],
-            'link_label': row['link_label'],
-            'rism': row['rism'],
-            'gore': row['gore'],
-            'locations': row['locations'],
+            'source_id':row['source_id'],
+            'full_title':row['full_title'],
+            'short_title':row['short_title'].strip(),
+            'author': author,
+            'description':row['source_description'],
+            'source_notes':row['source_detailed_notes'],
+            'publisher':row['published'],
+            'printer':row['printed'],
+            'edition':row['edition'],
+            'orientation':row['orientation'],
+            'date':row['date'],
+            'link':row['link_url'],
+            'link_label':row['link_label'],
+            'rism':row['rism'],
+            'gore':row['gore'],
+            'locations':row['locations'],
         }
 
         s = Source(**d)
@@ -71,14 +74,14 @@ class Command(BaseCommand):
         source = self.this_source
 
         d = {
-            'folder': row['folder'],
-            'pagination': row['pagination'],
-            'dimensions': row['dimensions'],
-            'library': row['library_name'],
-            'shelfmark': row['shelfmark'],
-            'item_notes': row['item_notes'],
+            'folder':row['folder'],
+            'pagination':row['pagination'],
+            'dimensions':row['dimensions'],
+            'library':row['library_name'],
+            'shelfmark':row['shelfmark'],
+            'item_notes':row['item_notes'],
             'source': source,
-            'seller': row['sold'],
+            'seller':row['sold'],
         }
 
         i = Item(**d)
