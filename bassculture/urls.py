50a9ecc71a6b80c35b384629744e03d417fa2122
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-07-04 15:53:09 +0100

    Added tunes to urls; also added uploads (in debug mode) to urls

--- bassculture/urls.py
+++ bassculture/urls.py
@@ -1,28 +1,37 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from bassculture.views.home import HomeView
 from bassculture.views.source import SourceList, SourceDetail
 from bassculture.views.author import AuthorList, AuthorDetail
+from bassculture.views.tune import TuneList, TuneDetail
 from bassculture.views.item import ItemList, ItemDetail
 from bassculture.views.search import SearchView
 
 urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'bassculture.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
+    # Examples:
+    # url(r'^$', 'bassculture.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-                       url(r'^$', HomeView.as_view(), name='home'),
-                       url(r'^search/$', SearchView.as_view(), name="search"),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^search/$', SearchView.as_view(), name="search"),
 
-                       url(r'^authors/$', AuthorList.as_view(), name="author-list"),
-                       url(r'^author/(?P<pk>[a-zA-Z0-9]+)/$', AuthorDetail.as_view(), name="author-detail"),
+    url(r'^authors/$', AuthorList.as_view(), name="author-list"),
+    url(r'^author/(?P<pk>[a-zA-Z0-9]+)/$', AuthorDetail.as_view(), name="author-detail"),
 
-                       url(r'^items/$', ItemList.as_view(), name="item-list"),
-                       url(r'^item/(?P<pk>[a-zA-Z0-9]+)/$', ItemDetail.as_view(), name="item-detail"),
+    url(r'^tunes/$', TuneList.as_view(), name="tune-list"),
+    url(r'^tune/(?P<pk>[a-zA-Z0-9]+)/$', TuneDetail.as_view(), name="tune-detail"),
 
-                       url(r'^sources/$', SourceList.as_view(), name="source-list"),
-                       url(r'^source/(?P<pk>[a-zA-Z0-9]+)/$', SourceDetail.as_view(), name="source-detail"),
-                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       )
+    url(r'^items/$', ItemList.as_view(), name="item-list"),
+    url(r'^item/(?P<pk>[a-zA-Z0-9]+)/$', ItemDetail.as_view(), name="item-detail"),
+
+    url(r'^sources/$', SourceList.as_view(), name="source-list"),
+    url(r'^source/(?P<pk>[a-zA-Z0-9]+)/$', SourceDetail.as_view(), name="source-detail"),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
