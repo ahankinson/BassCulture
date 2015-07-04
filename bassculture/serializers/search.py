2c8aef92844c36ef71a5535980adf3ae7f61d6d3
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-07-04 13:19:24 +0100

    Updated search serializer

--- bassculture/serializers/search.py
+++ bassculture/serializers/search.py
@@ -1,16 +1,15 @@
+import os
 from rest_framework import serializers
 
-
 class SearchSerializer(serializers.Serializer):
-    class Meta:
-        fields = ('url', 'source_id')
-
     url = serializers.SerializerMethodField("record_url")
+    title = serializers.ReadOnlyField()
+    content_type = serializers.ReadOnlyField()
+    source_title = serializers.ReadOnlyField()
 
     def record_url(self, obj, *args, **kwargs):
-        print(obj)
-        return 'Luca'
-
-#    def to_representation(self, *args, **kwargs):
-#        print(args)
-#        print(kwargs)
+        request = self.context.get('request', None)
+        type = obj.model_name.lower()
+        pk = obj.pk
+        # This should always be relative to the root, not the current path.
+        return request.build_absolute_uri(os.path.join('/', type, pk))
