514afd613f2e15899c6415cdea9d65112648e285
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-07-04 15:57:05 +0100

    Updated haystack tune indexer

--- bassculture/search_indexes/tune_index.py
+++ bassculture/search_indexes/tune_index.py
@@ -5,10 +5,13 @@ from bassculture.models.tune import Tune
 class TuneIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr="name")
+    alternate_spellings = indexes.CharField(model_attr="alternate_spellings", null=True)
     source_title = indexes.CharField(model_attr="item__source__short_title", faceted=True)
+    source_date = indexes.CharField(model_attr="item__source__date")
+    source_edition = indexes.CharField(model_attr="item__source__edition")
+    full_source_title = indexes.CharField(model_attr="item__source__full_title")
     author = indexes.CharField(model_attr="item__source__author__name", faceted=True)
     item_library = indexes.CharField(model_attr="item__library")
-    # author = indexes.CharField(model_attr="author")
 
     def get_model(self):
         return Tune
