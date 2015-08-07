f9c0677a5a8ba829927e9bb01763a6adc639dc6d
Luca Guariento <lguariento@users.noreply.github.com>
2015-08-07 12:03:37 +0100

    Update settings.py

--- bassculture/settings.py
+++ bassculture/settings.py
@@ -136,7 +136,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'uploads')
 MEDIA_URL = "/uploads/"
 
-<<<<<<< HEAD
 # HAYSTACK_CONNECTIONS = {
 #     'default': {
 #         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
@@ -144,12 +143,9 @@ MEDIA_URL = "/uploads/"
 #     },
 # }
 
-SOLR_SERVER = "http://54.175.165.60:8080/bassculture-solr/"
-IIP_SERVER = "http://local.hms.scot:8001/fcgi-bin/iipsrv.fcgi"
-=======
+
 SOLR_SERVER = ''
 IIP_SERVER = "http://bassculture.info/iipsrv"
->>>>>>> b1e9c57088ed39b1258c6517641f3d9e526de908
 IIP_SERVER_IMAGE_PATH = "/srv/webapps/BassCulture/bassculture/static/images/"
 DIVA_OBJECT_DATA = "/static/json/"
 
