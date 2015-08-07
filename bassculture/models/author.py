326215be8e5fb784c3d826ea81d3c962c81e080d
Luca Guariento <luca.guariento@glasgow.ac.uk>
2015-08-07 16:24:44 +0100

    Trying to have the import_metadata script working and sending data to solr correctly. I've commented out extra lines in order to have only one filed (author), and not three,(author_surname, author_firstname, extra_info) - still no luck (getting bassculture.models.author.DoesNotExist: Author matching query does not exist).

--- bassculture/models/author.py
+++ bassculture/models/author.py
@@ -7,22 +7,21 @@ class Author(models.Model):
     class Meta:
         app_label = 'bassculture'
 
-    author_surname = models.CharField(max_length=255)
-    author_firstname = models.CharField(max_length=255)
-    extra_info = models.CharField(max_length=16)
+    author = models.CharField(max_length=255)
+#    author_firstname = models.CharField(max_length=255)
+#    extra_info = models.CharField(max_length=16)
     biographical_info = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return u"{0}, {1}, {2}".format(self.surname, self.name,
-                                       self.extra_info)
+        return u"{0}".format(self.author)
 
-    @property
-    def full_name(self):
-        if self.name:
-            return u"{0}, {1}, {2}".format(self.surname, self.name,
-                                           self.extra_info)
-        else:
-            return u"{0}".format(self.surname)
+    # @property
+    # def full_name(self):
+    #     if self.name:
+    #         return u"{0}, {1}, {2}".format(self.surname, self.name,
+    #                                        self.extra_info)
+    #     else:
+    #         return u"{0}".format(self.surname)
 
 @receiver(post_save, sender=Author)
 def solr_index(sender, instance, created, **kwargs):
@@ -31,7 +30,7 @@ def solr_index(sender, instance, created, **kwargs):
     import scorched
 
     si = scorched.SolrInterface(settings.SOLR_SERVER)
-    record = si.query(type="author", author_id="{0}".format(instance.author_id)
+    record = si.query(type="author", author_id="{0}".format(instance.id)
                       ).execute()  # checks if the record exists in solr
 
     if record:  # if it exists
@@ -41,10 +40,10 @@ def solr_index(sender, instance, created, **kwargs):
         'pk': '{0}'.format(instance.pk),
         'type': 'author',
         'id': str(uuid.uuid4()),
-        'author_id': instance.author_id,
-        'name': instance.author_firstname,
-        'surname': instance.author_surname,
-        'extra_info': instance.author_extra_info,
+        'author_id': instance.id,
+#        'name': instance.author_firstname,
+        'author': instance.author,
+#        'extra_info': instance.author_extra_info,
     }
 
     si.add(d)
