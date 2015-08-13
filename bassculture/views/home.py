31c6c734175827692bcf6307940de2256d1514d6
Luca Guariento <luca.guariento@glasgow.ac.uk>
2015-08-13 12:10:09 +0100

    added renderers.BrowsableAPIRenderer

--- bassculture/views/home.py
+++ bassculture/views/home.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import GenericAPIView
+from rest_framework import renderers
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.reverse import reverse
@@ -13,7 +14,8 @@ class HomeViewHTMLRenderer(CustomHTMLRenderer):
 
 class HomeView(GenericAPIView):
     serializer_class = SearchSerializer
-    renderer_classes = (JSONRenderer, HomeViewHTMLRenderer)
+    renderer_classes = (JSONRenderer, HomeViewHTMLRenderer,
+                        renderers.BrowsableAPIRenderer)
 
     def get(self, request, *args, **kwargs):
         return Response({
