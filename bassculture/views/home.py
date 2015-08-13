b7b36fe177002be90f46f10389bd762ee4be2f56
Luca Guariento <luca.guariento@glasgow.ac.uk>
2015-08-13 12:05:55 +0100

    added views to the response object (get method)

--- bassculture/views/home.py
+++ bassculture/views/home.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+from rest_framework.reverse import reverse
 
 from bassculture.renderers.custom_html_renderer import CustomHTMLRenderer
 from bassculture.serializers.search import SearchSerializer
@@ -15,4 +16,8 @@ class HomeView(GenericAPIView):
     renderer_classes = (JSONRenderer, HomeViewHTMLRenderer)
 
     def get(self, request, *args, **kwargs):
-        return Response({})
+        return Response({
+            'sources': reverse('source-list', request=request),
+            'items': reverse('item-list', request=request),
+            'authors': reverse('author-list', request=request)
+            })
