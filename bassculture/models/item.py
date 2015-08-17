d6603dc0166939321713e407c814f70418c3a775
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-08-17 17:40:47 -0400

    Fixing item model
    Some fields were not large enough and were getting truncated

--- bassculture/models/item.py
+++ bassculture/models/item.py
@@ -9,10 +9,10 @@ class Item(models.Model):
         app_label = 'bassculture'
 
     folder = models.CharField(max_length=128, blank=True, null=True)
-    pagination = models.CharField(max_length=16)
-    dimensions = models.CharField(max_length=16)
-    library = models.CharField(max_length=40, blank=True, null=True)
-    shelfmark = models.CharField(max_length=32)
+    pagination = models.CharField(max_length=255)
+    dimensions = models.CharField(max_length=128)
+    library = models.CharField(max_length=128, blank=True, null=True)
+    shelfmark = models.CharField(max_length=128)
     item_notes = models.TextField(blank=True, null=True)
     source = models.ForeignKey("bassculture.Source", related_name="items")
     seller = models.TextField()
@@ -64,13 +64,13 @@ def solr_index(sender, instance, created, **kwargs):
     import scorched
 
     si = scorched.SolrInterface(settings.SOLR_SERVER)
-    record = si.query(type="item", item_id="{0}".format(instance.id)
-                      ).execute()  # checks if the record already exists in solr
+    record = si.query(type="item", pk="{0}".format(instance.pk)).execute()  # checks if the record already exists in solr
 
     if record:  # if it does
         si.delete_by_ids([x['id'] for x in record])
 
     d = {
+        'pk': str(instance.pk),
         'type': 'item',
         'id': str(uuid.uuid4()),
         'item_id': instance.id,
