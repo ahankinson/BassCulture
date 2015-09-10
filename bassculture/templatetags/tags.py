a03e45f35d3d8946529fd7749983ab0dd7cf4a09
Luca Guariento <luca.guariento@glasgow.ac.uk>
2015-09-10 14:26:55 +0100

    Fixed 'previous' and 'next' disabled buttons, and fixed '&' and '"' caracters-removal in 'resoults found in'

--- bassculture/templatetags/tags.py
+++ bassculture/templatetags/tags.py
@@ -6,3 +6,13 @@ register = template.Library()
 @register.filter()
 def ampersand(value):
     return value.replace("&", "%26")
+
+
+@register.filter()
+def slash(value):
+    return value.replace("\\", "")
+
+
+@register.filter()
+def dquot(value):
+    return value.replace("\"", "")
