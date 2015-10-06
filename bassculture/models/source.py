15e2d2dea7fd0b6af9f061f39745417c45d00906
Luca Guariento <luca.guariento@glasgow.ac.uk>
2015-10-06 09:23:54 +0100

    no message

--- bassculture/models/source.py
+++ bassculture/models/source.py
@@ -31,7 +31,8 @@ class Source(models.Model):
     rism = models.CharField(max_length=128, blank=True, null=True)
     gore = models.CharField(max_length=128, blank=True, null=True)
     locations = models.TextField(blank=True, null=True)
-    synonyms = models.TextField(blank=True, null=True)
+    synonyms = models.CharField(max_length=256, blank=True,
+                                null=True)
 
     def __str__(self):
             if self.short_title:
